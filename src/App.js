cdae12fe2d68ba5f178da173033f5e2147865fb2
bashar-27 <bashar0nidal@gmail.com>
2023-12-10 19:57:23 +0300

    FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Home";
+import Employee from "./Emp/Employee";
+import SignIn from "./Auth/Admin";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import EmployeePanel from "./Emp/EmployeePanel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="App container">
+          <h3 className="d-flex justify-content-center m-3">Employee Info</h3>
+
+          <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+            <ul className="navbar-nav">
+              <li className="nav-item- m-1">
+                <NavLink
+                  className="btn btn-light btn-outline-primary"
+                  to="/home">
+                  Home
+                </NavLink>
+              </li>
+
+              <li className="nav-item- m-1">
+                <NavLink
+                  className="btn btn-light btn-outline-primary"
+                  to="/employee">
+                  Add Employee
+                </NavLink>
+              </li>
+              <li className="nav-item- m-1">
+                <NavLink
+                  className="btn btn-light btn-outline-primary"
+                  to="/admin">
+                  Log In{" "}
+                </NavLink>
+              </li>
+              <li className="nav-item- m-1">
+                <NavLink
+                  className="btn btn-light btn-outline-primary"
+                  to="/employeePanel">
+                  All Emp{" "}
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/employee" element={<Employee />} />
+            <Route path="/admin" element={<SignIn />} />
+            <Route path="/employeePanel" element={<EmployeePanel />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeeTable from "./Emp/EmployeeTable";
+
+function Home() {
+  return (
+    <div>
+      <EmployeeTable />
+    </div>
+  );
+}
+
+export default Home;
